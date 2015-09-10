92f4eac653ca05a60d2a9d43936b851d7677f90d
Francois Petitjean <francois.petitjean@monash.edu>
2015-09-10 11:07:52 +1000

    fix bug call function MML

--- src/explorer/ChordalysisModellingMML.java
+++ src/explorer/ChordalysisModellingMML.java
@@ -32,138 +32,135 @@ import weka.core.Instances;
 import weka.core.converters.ArffLoader.ArffReader;
 
 /**
- * This class searches a statistically significant decomposable model to explain a dataset. 
- * See paper "A statistically efficient and scalable method for log-linear analysis of high-dimensional data, ICDM 2014"
- * See paper "Scaling log-linear analysis to datasets with thousands of variables, SDM 2015"
+ * This class searches a statistically significant decomposable model to explain
+ * a dataset. See paper
+ * "A statistically efficient and scalable method for log-linear analysis of high-dimensional data, ICDM 2014"
+ * See paper
+ * "Scaling log-linear analysis to datasets with thousands of variables, SDM 2015"
+ * 
  * @see http://www.tiny-clues.eu/Research/
  */
-public class ChordalysisModellingMML{
+public class ChordalysisModellingMML {
 
-	int nbInstances;
-	double pValueThreshold;
-	DecomposableModel bestModel;
-	MessageLengthFactorialComputer computer;
-	protected Lattice lattice;
-	Instances dataset;
-	ArrayList<GraphAction> operationsPerformed;
-	MyPriorityQueue pq;
-	GraphActionScorer scorer;
-	
-	int maxNSteps = Integer.MAX_VALUE;
-	public void setMaxNSteps(int nSteps){
-		this.maxNSteps = nSteps;
-		System.out.println(maxNSteps);
-	}
+    int nbInstances;
+    double pValueThreshold;
+    DecomposableModel bestModel;
+    MessageLengthFactorialComputer computer;
+    protected Lattice lattice;
+    Instances dataset;
+    ArrayList<GraphAction> operationsPerformed;
+    MyPriorityQueue pq;
+    GraphActionScorer scorer;
 
-	/**
-	 * Default constructor
-	 * 
-	 * @param pValueThreshold
-	 *            minimum p-value for statistical consistency (commonly 0.05)
-	 */
-	public ChordalysisModellingMML(double pValueThreshold) {
-		this.pValueThreshold = pValueThreshold;
-		operationsPerformed = new ArrayList<GraphAction>();
-	}
+    int maxNSteps = Integer.MAX_VALUE;
 
-	/**
-	 * Launch the modelling
-	 * 
-	 * @param dataset
-	 *            the dataset from which the analysis is performed on
-	 */
-	public void buildModel(Instances dataset) {
-		buildModelNoExplore(dataset);
-		this.explore();
-	}
-	
-	public int getNbInstances() {
-		return nbInstances;
-	}
-	
-	public void buildModelNoExplore(Instances dataset) {
-		this.nbInstances = dataset.numInstances();
-		this.dataset = dataset;
-		int[] variables = new int[dataset.numAttributes()];
-		int[] nbValuesForAttribute = new int[variables.length];
-		for (int i = 0; i < variables.length; i++) {
-			variables[i] = i;
-			nbValuesForAttribute[i] = dataset.numDistinctValues(i);
-		}
-		this.lattice = new Lattice(dataset);
-		this.computer = new MessageLengthFactorialComputer(dataset.numInstances(), this.lattice);
-		this.scorer = new GraphActionScorerMML(nbInstances, computer);
-		this.bestModel = new DecomposableModel(variables, nbValuesForAttribute);
-		this.pq = new MyPriorityQueue(variables.length, bestModel, scorer);
-		for (int i = 0; i < variables.length; i++) {
-			for (int j = i + 1; j < variables.length; j++) {
-				pq.enableEdge(i, j);
-			}
-		}
-		
+    public void setMaxNSteps(int nSteps) {
+	this.maxNSteps = nSteps;
+	System.out.println(maxNSteps);
+    }
 
-	}
-	
-	/**
-	 * Launch the modelling
-	 * 
-	 * @param dataset the structure of the dataset which the analysis is performed
-	 * @param 
-	 * @throws IOException 
-	 * 
-	 */
-	public void buildModel(Instances dataset,ArffReader loader) throws IOException {
-		buildModelNoExplore(dataset, loader);
-		this.explore();
-	}
-	
-	public void buildModelNoExplore(Instances dataset,ArffReader loader) throws IOException {
-		this.dataset = dataset;
-		int[] variables = new int[dataset.numAttributes()];
-		int[] nbValuesForAttribute = new int[variables.length];
-		for (int i = 0; i < variables.length; i++) {
-			variables[i] = i;
-			nbValuesForAttribute[i] = dataset.numDistinctValues(i);
-		}
-		this.lattice = new Lattice(dataset,loader);
-		this.nbInstances = this.lattice.getNbInstances();
-		
-		
-		this.computer = new MessageLengthFactorialComputer(nbInstances, this.lattice);
-		this.scorer = new GraphActionScorerMML(nbInstances, computer);
-		this.bestModel = new DecomposableModel(variables, nbValuesForAttribute);
-		this.pq = new MyPriorityQueue(variables.length, bestModel, scorer);
-		for (int i = 0; i < variables.length; i++) {
-			for (int j = i + 1; j < variables.length; j++) {
-				pq.enableEdge(i, j);
-			}
-		}
-		
+    /**
+     * Default constructor
+     * 
+     * @param pValueThreshold
+     *            minimum p-value for statistical consistency (commonly 0.05)
+     */
+    public ChordalysisModellingMML(double pValueThreshold) {
+	this.pValueThreshold = pValueThreshold;
+	operationsPerformed = new ArrayList<GraphAction>();
+    }
 
+    /**
+     * Launch the modelling
+     * 
+     * @param dataset
+     *            the dataset from which the analysis is performed on
+     */
+    public void buildModel(Instances dataset) {
+	buildModelNoExplore(dataset);
+	this.explore();
+    }
+
+    public int getNbInstances() {
+	return nbInstances;
+    }
+
+    public void buildModelNoExplore(Instances dataset) {
+	this.nbInstances = dataset.numInstances();
+	this.dataset = dataset;
+	int[] variables = new int[dataset.numAttributes()];
+	int[] nbValuesForAttribute = new int[variables.length];
+	for (int i = 0; i < variables.length; i++) {
+	    variables[i] = i;
+	    nbValuesForAttribute[i] = dataset.numDistinctValues(i);
+	}
+	this.lattice = new Lattice(dataset);
+	this.computer = new MessageLengthFactorialComputer(dataset.numInstances(), this.lattice);
+	this.scorer = new GraphActionScorerMML(nbInstances, computer);
+	this.bestModel = new DecomposableModel(variables, nbValuesForAttribute);
+	this.pq = new MyPriorityQueue(variables.length, bestModel, scorer);
+	for (int i = 0; i < variables.length; i++) {
+	    for (int j = i + 1; j < variables.length; j++) {
+		pq.enableEdge(i, j);
+	    }
 	}
 
-	/**
-	 * @return the Decomposable model that has been built
-	 */
-	public DecomposableModel getModel() {
-		return bestModel;
+    }
+
+    /**
+     * Launch the modelling
+     * 
+     * @param dataset
+     *            the structure of the dataset which the analysis is performed
+     * @param
+     * @throws IOException
+     * 
+     */
+    public void buildModel(Instances dataset, ArffReader loader) throws IOException {
+	buildModelNoExplore(dataset, loader);
+	this.explore();
+    }
+
+    public void buildModelNoExplore(Instances dataset, ArffReader loader) throws IOException {
+	this.dataset = dataset;
+	int[] variables = new int[dataset.numAttributes()];
+	int[] nbValuesForAttribute = new int[variables.length];
+	for (int i = 0; i < variables.length; i++) {
+	    variables[i] = i;
+	    nbValuesForAttribute[i] = dataset.numDistinctValues(i);
+	}
+	this.lattice = new Lattice(dataset, loader);
+	this.nbInstances = this.lattice.getNbInstances();
+
+	this.computer = new MessageLengthFactorialComputer(nbInstances, this.lattice);
+	this.scorer = new GraphActionScorerMML(nbInstances, computer);
+	this.bestModel = new DecomposableModel(variables, nbValuesForAttribute);
+	this.pq = new MyPriorityQueue(variables.length, bestModel, scorer);
+	for (int i = 0; i < variables.length; i++) {
+	    for (int j = i + 1; j < variables.length; j++) {
+		pq.enableEdge(i, j);
+	    }
 	}
 
-	public void explore() {
-		pq.processStoredModifications();
-		int step=0;
-		while (!pq.isEmpty() && step<maxNSteps) {
-			int nbTests = pq.size();
-			double correctedPValueThreshold = (pValueThreshold / Math.pow(2, step)) / nbTests;
+    }
 
-			ScoredGraphAction todo = pq.poll();
-			if (todo.getScore()>= correctedPValueThreshold) {
-				break;
-			}
-			operationsPerformed.add(todo);
-			bestModel.performAction(todo, bestModel, pq);
-			step++;
-		}
+    /**
+     * @return the Decomposable model that has been built
+     */
+    public DecomposableModel getModel() {
+	return bestModel;
+    }
+
+    public void explore() {
+	pq.processStoredModifications();
+	while (!pq.isEmpty()) {
+	    ScoredGraphAction todo = pq.poll();
+	    if (todo.getScore() > 0.0) {
+		break;
+	    }
+	    operationsPerformed.add(todo);
+	    bestModel.performAction(todo, bestModel, pq);
 	}
+    }
 
 }
